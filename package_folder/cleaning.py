f0f7d54ac4b4b3be9176e46aa02f185a9158f9a5
Raphael Rousseau <raphael@rousseauweb.net>
2024-06-10 18:00:56 -0400

    structure and ML ops

--- package_folder/cleaning.py
+++ package_folder/cleaning.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-data = pd.read_csv("../raw_data/ds_salaries.csv")
+#Changing the structure : loading directly into the models.py file
+#data = pd.read_csv("../raw_data/ds_salaries.csv")
 
 #delete duplicate
 def delete_duplicates (data):
@@ -76,3 +77,4 @@ def group_countries(data):
     # Count the number of entries for each region and high-entry country
     region_counts = data['company_location_grouped'].value_counts()
     print(region_counts)
+    return data
